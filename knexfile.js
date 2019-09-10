284b7169f4a6cea9c7262d2272d1318c8d474300
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-09-09 20:00:27 -0700

    knexfile prod profile change

--- knexfile.js
+++ knexfile.js
@@ -21,29 +21,22 @@ module.exports = {
     }
   },
 
-  testing: {
+  production: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/testbookr.db3'
-    },
     useNullAsDefault: true,
+    connection: {
+      filename: './database/bookr.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
     migrations: {
       directory: './database/migrations'
     },
     seeds: {
       directory: './database/seeds'
     }
-  },
-
-  production: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/bookr.db3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: { directory: './database/seeds' }
   }
 };
